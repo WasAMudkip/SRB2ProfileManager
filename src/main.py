c21b2319c6339c33834781c9d088c156b05ed8d2
WasAMudkip <267760571+WasAMudkip@users.noreply.github.com>
2026-05-02 23:58:13 +0200

    art go brrr

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("PLACEHOLDER")
+print("""
+=========================
+=  NotAMudkip's         =
+=      SONIC  ROBO      =
+=        BLAST 2        =
+=      Mod Profile      =
+=        Manager v1.0.0 =
+=========================
+""")
