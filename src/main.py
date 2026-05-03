22d3e85d064e9f514c74318897807a62fd3237bb
WasAMudkip <267760571+WasAMudkip@users.noreply.github.com>
2026-05-03 09:10:42 +0000

    basic stuff

--- src/main.py
+++ src/main.py
@@ -6,4 +6,12 @@ print("""
 =      Mod Profile      =
 =        Manager v1.0.0 =
 =========================
+
+Modes:
+0 - exit
+1 - list mode
+2 - create shortcut from addons
 """)
+
+mode = int(input("Select a mode: "))
+print(mode)
